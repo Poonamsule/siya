7bad0d9962dd797f3f837636bd5d980d3c0f1ff4
Poonamsule <poonamsonwalkar@gmail.com>
2020-08-03 01:15:16 +0530

    first commit

---
+++ src/main/java/com/poonam/yy/SiyaApplication.java
@@ -0,0 +1,16 @@
+package com.poonam.yy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiyaApplication.class, args);
+	}
+
+	
+	
+	
+}
---
+++ src/test/java/com/poonam/yy/SiyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poonam.yy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
